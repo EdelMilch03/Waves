07295d434065e03fe100aef0e07fb8d0002844e0
Ringeeee <genzelaraa@gmail.com>
2025-11-18 17:22:20 +0100

    Start Room, Character Movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,16 +14,25 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("Left", "Right")
+
+
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite.play("jump")
+
+	if direction == 0 and is_on_floor() :
+		animated_sprite.play("idle")
 
 	move_and_slide()
 	_handle_direction(direction)
